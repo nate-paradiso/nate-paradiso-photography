fa508cb8f29cdf1cb7e52026a1853e89973db9f3
Nate Paradiso <ipfcolo05@gmail.com>
2024-02-09 16:23:55 -0800

    not working- must pay for hosting...

---
+++ src/_utility/utility.js
@@ -0,0 +1,29 @@
+export const formatTimeFromNow = timestamp => {
+  const timeNow = Date.now();
+  const secondsAgo = Math.floor((timeNow - timestamp) / 1000);
+  if (secondsAgo < 60) {
+    return `${secondsAgo} second${secondsAgo !== 1 ? "s" : ""} ago`;
+  }
+  const minutesAgo = Math.floor(secondsAgo / 60);
+  if (minutesAgo < 60) {
+    return `${minutesAgo} minute${minutesAgo !== 1 ? "s" : ""} ago`;
+  }
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  if (hoursAgo < 24) {
+    return `${hoursAgo} hour${hoursAgo !== 1 ? "s" : ""} ago`;
+  }
+  const daysAgo = Math.floor(hoursAgo / 24);
+  if (daysAgo < 7) {
+    return `${daysAgo} day${daysAgo !== 1 ? "s" : ""} ago`;
+  }
+  const weeksAgo = Math.floor(daysAgo / 7);
+  if (weeksAgo < 4) {
+    return `${weeksAgo} week${weeksAgo !== 1 ? "s" : ""} ago`;
+  }
+  const monthsAgo = Math.floor(daysAgo / 30);
+  if (monthsAgo < 12) {
+    return `${monthsAgo} month${monthsAgo !== 1 ? "s" : ""} ago`;
+  }
+  const yearsAgo = Math.floor(monthsAgo / 12);
+  return `${yearsAgo} year${yearsAgo !== 1 ? "s" : ""} ago`;
+};
