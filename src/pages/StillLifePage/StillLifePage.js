562d6b0eb8d8f3b883ae7ecd45f644aa7c75b2da
Nate Paradiso <ipfcolo05@gmail.com>
2024-03-08 10:30:25 -0800

    saved stilllife page

--- src/pages/StillLifePage/StillLifePage.js
+++ src/pages/StillLifePage/StillLifePage.js
@@ -4,18 +4,6 @@ import "./StillLifePage.scss";
 
 export const StillLifePage = () => {
   return (
-    // <div className="still-life">
-    //   <img
-    //     className="still-life__img"
-    //     src="https://res.cloudinary.com/hzjhihahh/image/upload/v1706940277/portfolio-nate-paradiso-photography/tyzjtcvqbv0gjbp9c1bn.jpg"
-    //     alt="Milk n Cookies"
-    //   />
-    //   <img
-    //     className="still-life__img"
-    //     src="https://res.cloudinary.com/hzjhihahh/image/upload/v1709921307/portfolio-nate-paradiso-photography/rr3rx9hbgrs01ngonjz1.jpg"
-    //     alt="Milk n Cookies 2"
-    //   />
-    // </div>
     <div className="still-life">
       <Gallery photos={cloudinaryStillLifeSlides} layout="columns" columns="2" margin="31.25rem" />
     </div>
