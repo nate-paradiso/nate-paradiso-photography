ffb64fa3b727b4f757be27df6c2fe683a8859e83
Nate Paradiso <ipfcolo05@gmail.com>
2024-03-14 14:20:31 -0700

    added banner and title

--- src/pages/WeddingPage/WeddingPage.js
+++ src/pages/WeddingPage/WeddingPage.js
@@ -1,9 +1,11 @@
 import { Gallery } from "../../components/Gallery/Gallery";
 import { cloudinaryWeddingSlides } from "../../components/Slides/Slides";
+import "./WeddingPage.scss";
 
 export const WeddingPage = () => {
   return (
     <>
+      <p className="wedding-title">weddings</p>
       <Gallery photos={cloudinaryWeddingSlides} layout="columns" />
     </>
   );
